e5e77956fb5ff02f235efcda1dcb73a6783f57cc
Qin Xuzhou <xuzhou.qin@jcdecaux.com>
2018-01-25 13:19:42 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+from setuptools import setup
+
+from pyofo import __version__
+
+setup(
+    name="pyofo",
+    version=__version__.__version__,
+    author=__version__.__author__,
+    author_email=__version__.__author_email__,
+    packages=["pyofo"],
+    license="LICENSE",
+    description="A Python library to interface with ofo bike API",
+    install_requires=[
+        'requests',
+    ],
+)
