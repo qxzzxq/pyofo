28c39a5b0c669aeb03ef5160b00ae034fcc216ce
qin <xuzhou.qin@gmail.com>
2018-01-26 00:31:43 +0100

    edit setup

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ from setuptools import setup
 from pyofo import __version__
 
 setup(
-    name="pyofo",
+    name='pyofo',
     version=__version__.__version__,
     author=__version__.__author__,
     author_email=__version__.__author_email__,
-    packages=["pyofo"],
-    license="MIT",
-    description="A Python library to interface with ofo bike API",
+    url='https://github.com/qxzzxq/pyofo',
+    packages=['pyofo'],
+    license='MIT',
+    description='A Python library to interface with ofo bike API',
     install_requires=[
         'requests',
     ],
