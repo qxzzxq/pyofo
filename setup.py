aabdb7ca4aa68db37b016bfd7ae32fa18698f1bc
Xuzhou Qin <xuzhou.qin@gmail.com>
2018-07-30 22:10:46 +0200

    0.1.2 fix import error

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import __version__
 
 
 setup(
-    name='pyofo',
+    name=__version__.__title__,
     version=__version__.__version__,
     author=__version__.__author__,
     author_email=__version__.__author_email__,
